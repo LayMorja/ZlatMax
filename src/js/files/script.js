120c21be68b09456712a2a2893668a3cc7e29884
LayMorja <evil-corporaty@yandex.ru>
2022-02-09 20:55:36 +0300

    Первая проба подключения к GitHub

---
+++ src/js/files/script.js
@@ -0,0 +1,58 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+// import { flsModules } from "./modules.js";
+
+document.addEventListener('click', documentActions);
+
+const menuBlocks = document.querySelectorAll('.sub-menu-catalog__block');
+if(menuBlocks.length) {
+   menuBlocks.forEach(menuBlock => {
+      const menuBlockItems = menuBlock.querySelectorAll('.sub-menu-catalog__category').length;
+      menuBlock.classList.add(`sub-menu-catalog__block_${menuBlockItems}`);
+   });
+}
+
+function documentActions(e) {
+   const targetElement = e.target;
+   if(targetElement.closest('[data-parent]')) { // если таргет = элементу с определенным свойством
+      const subMenuId = targetElement.dataset.parent ? targetElement.dataset.parent : null; // переменная номера блока подменю
+      const subMenu = document.querySelector(`[data-submenu="${subMenuId}"]`); // переменная самого подменю под таким номером
+
+      if(subMenu) {
+         const activeLink = document.querySelector('._sub-menu-active');
+         const activeBlock = document.querySelector('._sub-menu-open');
+
+         if(activeLink && activeLink !== targetElement) {
+            activeLink.classList.remove('_sub-menu-active');
+            activeBlock.classList.remove('_sub-menu-open');
+            document.documentElement.classList.remove('sub-menu-open');
+         }
+
+         document.documentElement.classList.toggle('sub-menu-open');
+         targetElement.classList.toggle('_sub-menu-active');
+         subMenu.classList.toggle('_sub-menu-open');
+      } else {
+         console.log('Такого подменю нет');
+      }
+
+      e.preventDefault();
+   }
+   if(targetElement.closest('.menu-top-header__link_catalog')) {
+      document.documentElement.classList.add('catalog-open');
+      e.preventDefault();
+   }
+   if(targetElement.closest('.menu-catalog__back')) { // если таргет = закрывающей иконке
+      document.documentElement.classList.remove('catalog-open'); // меняем класс у документа
+      document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+      document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-active') : null;
+      e.preventDefault();
+   }
+   
+   if(targetElement.closest('.sub-menu-catalog__back')) { // если таргет = закрывающей иконке
+      document.documentElement.classList.remove('sub-menu-open');
+      document.querySelector('._sub-menu-active') ? document.querySelector('._sub-menu-active').classList.remove('_sub-menu-active') : null;
+      document.querySelector('._sub-menu-open') ? document.querySelector('._sub-menu-open').classList.remove('_sub-menu-open') : null;
+      e.preventDefault();
+   }
+}
